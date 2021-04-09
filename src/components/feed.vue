0d550b7126c4875b0501c7c03355f575d794afba
amitasher22 <amitasher4@gmail.com>
2021-04-09 12:05:23 +0300

    first commit

---
+++ src/components/feed.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <q-page class="flex-center">
+      <h5>home page</h5>
+    </q-page>
+  </div>
+
+</template>
+
+<script>
+export default {
+name: "feed"
+}
+</script>
+
+<style scoped>
+
+</style>
