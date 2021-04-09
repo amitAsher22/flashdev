c580a10ee66620026fe38d71eb7408d99fb55797
amitasher22 <amitasher4@gmail.com>
2021-04-09 12:22:45 +0300

    first commit

--- src/components/Inputs.vue
+++ src/components/Inputs.vue
@@ -1,104 +1,97 @@
 <template>
-  <div style="width:800px ; margin: auto ">
-
-
+  <div style="width:800px ; margin: auto " >
     <h4 style="text-align: center">מלא פרטים על בעל העסק</h4>
-    <q-input outlined v-model="editedItem.name" label="שם בעל העסק"  />
-    <q-input outlined v-model="editedItem.email" label="איימל"/>
-    <q-input outlined v-model="editedItem.phone" label="מספר טלפון" type="number"/>
-    <q-input outlined v-model="editedItem.address" label="כתובת בעל העסק"/>
-    <q-input outlined v-model="editedItem.profession" label="מקצוע"/>
-
+    <q-input  @dblclick="search1()" v-model="textInput" placeholder="חיפוש"/>
+    <q-input outlined v-model="localEditedItem.name" label="שם בעל העסק"/>
+    <q-input outlined v-model="localEditedItem.email" label="איימל"/>
+    <q-input outlined v-model="localEditedItem.phone" label="מספר טלפון" type="number"/>
+    <q-input outlined v-model="localEditedItem.address" label="כתובת בעל העסק"/>
+    <q-input outlined v-model="localEditedItem.profession" label="מקצוע"/>
     <q-file filled bottom-slots v-model="model" label="הוסף תמונה" counter max-files="12">
       <template v-slot:append>
-        <q-icon v-if="model !== null" name="close" @click.stop="model = null" class="cursor-pointer" />
-        <q-icon name="create_new_folder" @click.stop />
+        <q-icon v-if="model !== null" name="close" @click.stop="model = null" class="cursor-pointer"/>
+        <q-icon name="create_new_folder" @click.stop/>
       </template>
 
-      <template v-slot:after>
-        <q-btn round dense flat icon="send" />
-      </template>
     </q-file>
 
 
-
-    <q-btn v-if="editedItem" color="blue" text-color="black" label="הוסף" @click="Add()"/>
-    <q-btn v-if="!editedItem" color="yellow" text-color="black" label="עדכון" @click="update()"/>
+    <q-btn v-if="!localEditedItem.id" color="blue" text-color="black" label="הוסף" @click="insert()"/>
+    <q-btn v-if="localEditedItem.id" color="yellow" text-color="black" label="עדכון" @click="update()"/>
   </div>
 </template>
 
 
-
-
-
-
 <script>
 
-// import localStorageDriver from '@/middleware/localStorage/profile-items_xx'
-// import api from '@/middleware/api'
-import Firebasedatabase from '../middleware/firebase/database/index'
-import firebaseInstance from '../middleware/firebase'
+import {mapActions, mapState, mapMutations} from 'vuex'
 
 export default {
   name: "Inputs",
-  props: ['tableName', 'item','ownerId' ],
+  props: ['tableName'],
+  computed: mapState('owners', ['editedOwner', 'Owners', 'textFirst']),
 
   data() {
     return {
-      editedItem: {
+      textInput: '',
+      localEditedItem: {
         name: '',
-        email:'',
+        email: '',
         phone: '',
         address: '',
         profession: '',
-        img:'',
-
+        img: '',
       },
-      condition:false,
+
       model: null
 
     }
   },
-  methods: {
-    Add() {
-      firebaseInstance.firebase.storage().ref(`Owners/${window.user.uid}/Images/${this.model.name}`).put(this.model)
-         .then((storageRef)=> {
-           storageRef.ref.getDownloadURL()
-               .then((url) => {
-               this.editedItem.img = url
-               })
-           .then(()=>{
-             Firebasedatabase.create({entity: this.tableName, Owner: this.editedItem})
-                 .then(() => {
-                   this.$emit('itemAdded');
-                 })
-           })
 
-         })
+  methods: {
+
+    ...mapActions('owners', ['insertOwner', 'updateOwner', 'setEditOwnerById', 'searchFromWOrd' , 'searchFromWOrd' ]),
+    ...mapMutations('owners', ['setEditOwner', 'setEditOwnerId', 'editOwner']),
+
+    localSetEditedOwner() {
+      this.setEditOwner(this.localEditedItem)
+    },
+    goToHome() {
+      this.$router.push('/home');
+    },
+
+    insert() {
+      this.localSetEditedOwner();
+      this.insertOwner()
 
     },
 
     update() {
-      Firebasedatabase.update({entity: this.tableName,  id: this.ownerId,  Owner: this.editedItem})
-      .then(()=>{
-        this.$router.push('/home');
-      })
+      this.localSetEditedOwner();
+      this.updateOwner();
+      this.goToHome();
+    },
+
+    search1() {
+      this.searchFromWOrd(this.textInput )
     }
   },
 
+
   created() {
-    if (this.item) {
-      this.condition = true
-      this.editedItem = this.item;
+    if (this.$route.params.id) {
+      this.setEditOwnerId(this.$route.params.id)
+      this.setEditOwnerById()
+          .then(() => {
+            Object.assign(this.localEditedItem, this.editedOwner)
+          })
     }
   }
+
 }
 
+
 </script>
-
-
-
-
 
 
 <style>
